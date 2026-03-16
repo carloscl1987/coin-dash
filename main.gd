9ddbc96c3954327b1e46fffae691586ff3d266e9
10556324 <carlos.contreras@autozone.com>
2026-03-16 00:10:06 -0600

    wip

--- main.gd
+++ main.gd
@@ -31,3 +31,9 @@ func spawn_coins():
 		#c.screensize = screensize
 		c.position = Vector2(randi_range(0, screensize.x),
 			randi_range(0, screensize.y))
+			
+func _process(delta: float) -> void:
+	if playing and get_tree().get_nodes_in_group("coins").size() == 0:
+		level += 1
+		time_left += 5
+		spawn_coins()
